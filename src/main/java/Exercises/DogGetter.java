43eccef4118a540be79511fe5b77e17e79ba0799
ArnoldasG <grevasarnoldas@gmail.com>
2020-11-17 17:22:41 +0200

    Simple exercise with two variables how to use getter and setter.

---
+++ src/main/java/Exercises/DogGetter.java
@@ -0,0 +1,29 @@
+package Exercises;
+
+public class DogGetter {
+    public String name;
+    public int age;
+
+
+    public String getName(){
+        return name;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setAge(int age){
+        this.age = age;
+    }
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
