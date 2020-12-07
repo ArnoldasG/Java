750969119a1d6deeee6ca87d6922fca121486d9f
ArnoldasG <74469109+ArnoldasG@users.noreply.github.com>
2020-12-07 13:10:01 +0200

    Add files via upload

---
+++ src/main/java/Exercises/OOP/Constructors/LessonBankAccount/Main.java
@@ -0,0 +1,37 @@
+package Exercises.OOP.Constructors.LessonBankAccount;
+/*
+
+public class Main {
+    public static void main(String[] args) {
+
+        Account ArnoldasAccount = new Account();
+        ArnoldasAccount.withdrawal(100.0);
+
+        ArnoldasAccount.deposit(50.0);
+        ArnoldasAccount.withdrawal(100.0);
+
+        ArnoldasAccount.deposit(51.0);
+        ArnoldasAccount.withdrawal(100.0);
+
+        System.out.println("\n\n");
+
+        Account GrevasAccount = new Account("33425", 550.0, "Arnoldas Grevas", "mail@mail.lt",
+                "861445510");
+        System.out.println("Account nubmber: " + GrevasAccount.getNumber());
+        System.out.println("Account Ballance: " + GrevasAccount.getBalance());
+
+        System.out.println("\nDeposit: 394 eur.");
+        GrevasAccount.deposit(394.0);
+
+        System.out.println("\n\n\nExercise with VIP Person\n");
+        VipPerson person1 = new VipPerson();
+        System.out.println(person1.getName());
+        VipPerson person2 = new VipPerson("Arnoldas", 5000);
+        System.out.println("Name:   " + person2.getName() + "   Credit Limit: " + person2.getCreditLimit());
+        VipPerson person3 = new VipPerson("Grevas", 10000, "grevas@mail.com");
+        System.out.println("Name:   " + person3.getName() + "   Credit Limit: " + person3.getCreditLimit() + "   Email address "
+        + person3.getEmailAddress());
+
+    }
+}
+*/
