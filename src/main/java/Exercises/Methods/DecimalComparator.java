4e1b7d895698b37d2b1d12534d394d360a546ef2
ArnoldasG <grevasarnoldas@gmail.com>
2020-12-09 11:26:22 +0200

    Decimal numbers Coparator

---
+++ src/main/java/Exercises/Methods/DecimalComparator.java
@@ -0,0 +1,12 @@
+package main.java.Exercises.Methods;
+
+public class DecimalComparator {
+
+    public static boolean areEqualThreeDecimalPlaces(double one, double two) {
+
+        if ((int) (one * 1000) == (int) (two * 1000)) {
+            return true;
+        }
+        return false;
+    }
+}
