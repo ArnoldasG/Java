c4f4afd90c72a98ca6ec1d305393bc2d142c2f16
ArnoldasG <grevasarnoldas@gmail.com>
2020-12-23 02:50:03 +0200

    Composition classes and how to use it.

---
+++ src/main/java/Exercises/OOP/Constructors/Explanation/Main.java
@@ -0,0 +1,18 @@
+package main.java.Exercises.OOP.Constructors.Explanation;
+
+public class Main {
+    public static void main(String[] args) {
+        Dimensions dimensions = new Dimensions(20, 20, 5);
+        Case theCase = new Case("220B", "MSI", "240", dimensions);
+
+
+        Monitor theMonitor = new Monitor("27cm", "AOC", 27, new Resolution(2540, 1440));
+
+        Motherboard theMotherboard = new Motherboard("3080", "RTX", 16, 6, "v2.44");
+
+        PC thePC = new PC(theCase, theMonitor, theMotherboard);
+        thePC.getMonitor().drawPixelAt(1500, 1200, "red");
+        thePC.getMotherboard().loadProgram("Macintosh");
+        thePC.getTheCase().pressPowerButton();
+    }
+}
