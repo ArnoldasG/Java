b4fde438ef7e9b42cb2600370c227dec2ebbbf48
ArnoldasG <grevasarnoldas@gmail.com>
2020-12-23 02:04:28 +0200

    Carpet calculation with three classes, methods and constructors.

---
+++ src/main/java/Exercises/OOP/Inheritence/Exercises/Main.java
@@ -0,0 +1,10 @@
+package main.java.Exercises.OOP.Inheritence.Exercises;
+
+public class Main {
+    public static void main(String[] args) {
+        Porsche Cayenne = new Porsche(4, "Cayenne", 5, false, 8, "Black", "Full Led", 12);
+        Cayenne.steer(45);
+        Cayenne.accelerate(66);
+
+    }
+}
