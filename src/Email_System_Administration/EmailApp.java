610ba4f3d6660ce0fde3711a7bb3d3436824cbc4
ArnoldasG <grevasarnoldas@gmail.com>
2021-02-22 12:49:51 +0200

    Simple email system

---
+++ src/Email_System_Administration/EmailApp.java
@@ -0,0 +1,11 @@
+package Email_System_Administration;
+
+public class EmailApp {
+
+    public static void main(String[] args) {
+	// write your code here
+        Email email = new Email("Arnoldas", "Grevas");
+        System.out.println(email.showInfo());
+
+    }
+}
--- src/Email_System_Administration/Main.java
+++
@@ -1,9 +0,0 @@
-package Email_System_Administration;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-        System.out.println("We have some changes over here.. :)");
-    }
-}
